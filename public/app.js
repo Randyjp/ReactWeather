1b5a1e54f195e540d5e768d6bf951b16519af133
randyjp <randyjp125@gmail.com>
2017-01-14 14:28:59 -0500

    adding and simple test of webapck 1

---
+++ public/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by rperez on 1/14/17.
+ */
+var greeter = require('./components/Greeter')
+greeter();
---
+++ public/components/Greeter.js
@@ -0,0 +1,8 @@
+/**
+ * Created by rperez on 1/14/17.
+ */
+function greeter() {
+    document.write("from greeter function")
+}
+
+module.exports = greeter;
