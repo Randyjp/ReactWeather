690bf8a44e7a5815b6ade47ceb6f475dff4cd439
randyjp <randyjp125@gmail.com>
2017-01-26 17:33:47 -0500

    Setitng up the app to use heroku

--- server.js
+++ server.js
@@ -3,9 +3,24 @@
  */
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+//fowards all https traffic to http
+//this is because open weather app free is
+//only on http
+app.use(function (req, res, next) {
+  //checks the protocol
+  if (req.header['x-forwarded-proto'] === 'http') {
+    //if http just keep going
+    next();
+  } else {
+    //if https, redirect to http
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public')); //exposed folder
 
-app.listen(3000, function () { //Starts a UNIX socket and listens for connections on the given path.
-   console.log('Express server is up and running in port 3000')
+app.listen(PORT, function () { //Starts a UNIX socket and listens for connections on the given path.
+   console.log('Express server is up and running in port ' + PORT);
 });
