6a90007073fda286c1e13e185e1d4c4d1595baae
randyjp <randyjp125@gmail.com>
2017-01-28 16:39:33 -0500

    adding the foundation grid and styling examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Examples = React.createClass({
 //   render: function () {
@@ -11,8 +12,16 @@ var React = require('react');
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few exampl locations to try out</p>
+      <ol>
+        <li>
+          <Link to='/?location=Alexandria'>Alexandria, VA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
     </div>
   );
 }
