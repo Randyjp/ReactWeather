de17a746e2f04abb27c4271a12fd498ffc5c2670
randyjp <randyjp125@gmail.com>
2017-01-30 19:53:17 -0500

    fixing the modal error

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMSever = require('react-dom/server');
 
 var ErrorModal = React.createClass({
   getDefaultProps: function () {
@@ -16,13 +18,11 @@ var ErrorModal = React.createClass({
   //gets called by react after the DOM is updated with the content of the
   //render function. In this case it shows the modal
   componentDidMount: function (){
-    var modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();
-  },
-  render: function () {
     var {title, message} = this.props;
-
-    return (
+    //had to move the markup here because foundation was altering the DOM
+    //this doesn't work well with react because react it's trying to maintain
+    //the state of the DOM. Solution: use Jquery for the DOM is this case
+    var modalMarkup = (
       <div className="reveal tiny text-center" id="error-modal" data-reveal="">
         <h4>{title}</h4>
         <p>{message}</p>
@@ -33,6 +33,31 @@ var ErrorModal = React.createClass({
         </p>
       </div>
     );
+
+    var $modal = $(ReactDOMSever.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+  render: function () {
+    // var {title, message} = this.props;
+    return (
+      <div>
+
+      </div>
+    );
+    // return (
+    //   <div className="reveal tiny text-center" id="error-modal" data-reveal="">
+    //     <h4>{title}</h4>
+    //     <p>{message}</p>
+    //     <p>
+    //       <button className="button hollow" data-close="">
+    //         Okay
+    //       </button>
+    //     </p>
+    //   </div>
+    // );
   }
 });
 
